65c8f1d3770c811897b16e420f2ad9113f1fdb7b
fkgrkyr <fkgrkyr@gmail.com>
2025-09-24 01:56:27 +0900

    chore: 초기 배포용 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "@/pages/Home"
+// import Home from "@/pages/Home"
 import ChatRoom from "@/pages/ChatRoom"
 
 const App = () => {
 
   return (
-    <div className="min-h-screen bg-[##F5F5F5] flex justify-center">
+    <div className="min-h-screen bg-[#F5F5F5] flex justify-center">
       <div className="w-[375px] min-h-screen bg-white shadow-2xl">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/chatroom" element={<ChatRoom />} />
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<ChatRoom />} />
           </Routes>
         </BrowserRouter>
       </div>
