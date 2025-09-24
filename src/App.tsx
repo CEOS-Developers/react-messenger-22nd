e8ce3afb940b9d8f23ea56e5698015b3b3dc27d8
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-09-24 16:40:50 +0900

    chore: 개발 환경 설정

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello React + Tailwind + TS 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
