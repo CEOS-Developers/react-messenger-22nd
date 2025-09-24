4546dfa07ddecf87d2082aa8eaa9578bf9035dfd
baekseungsun <baekjimmy@yonsei.ac.kr>
2025-09-25 01:27:42 +0900

    feat: dateCalculation 수정

--- src/utils/dateCalculation.tsx
+++ src/utils/dateCalculation.tsx
@@ -2,6 +2,6 @@
 
 export function formatTodayMD(compact = false, tz = 'Asia/Seoul') {
     const s = new Intl.DateTimeFormat('ko-KR', { timeZone: tz, month: 'long', day: 'numeric' })
-        .format(new Date());              // 예: "9월 24일"
+        .format(new Date());
     return compact ? s.replace(/\s+/g, '') : s;
 }
