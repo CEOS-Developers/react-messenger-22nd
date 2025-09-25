323ff013769fa3fd54c5790622db36b5857747b3
fkgrkyr <fkgrkyr@gmail.com>
2025-09-26 06:13:36 +0900

    fix: 배포 오류 재수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,8 @@ const Home = () => {
             <Header 
             type="home"
              />
-             <button className="mt-10 ml-10 px-4 py-2 bg-green-3 text-white rounded">
-              <a href="https://react-messenger-22nd-five.vercel.app/chatroom/1" target="_blank" rel="noopener noreferrer">채팅방 이동</a>
-             </button>
         </div>
-    );
+    );  
 }
 
 export default Home;
