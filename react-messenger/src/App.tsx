3e4c11ab43bf36613a2b21460b59ae3abd6371a1
hammoooo <chlangjs@g.hongik.ac.kr>
2025-09-26 10:28:12 +0900

    feat: 초기 세팅 및 헤더

---
+++ react-messenger/src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="mx-auto w-[375px] min-h-screen bg-white border border-gray-200">
+      <Header />
+      <div className="divider" />
+
+      <main>
+        {/* <Friends /> */}
+
+        {/* <ChatRoom /> */}
+      </main>
+
+      {/* Tab bar */}
+    </div>
+  );
+}
---
+++ react-messenger/src/components/Header.tsx
@@ -0,0 +1,27 @@
+export default function Header() {
+  return (
+    <header className="p-4 flex justify-between items-center">
+      <h1 className="text-2xl font-bold">친구</h1>
+      <div className="flex space-x-4 ">
+        {[
+          { src: "/images/magnifying.svg", alt: "검색" },
+          { src: "/images/music.svg", alt: "뮤직" },
+          { src: "/images/plusHuman.svg", alt: "친구추가" },
+          { src: "/images/setting.svg", alt: "설정" },
+        ].map((i) => (
+          <button
+            key={i.src}
+            type="button"
+            className="w-6 h-[24px] active:scale-95 transition"
+          >
+            <img
+              src={i.src}
+              alt={i.alt}
+              className="w-5 h-5 pointer-events-none"
+            />
+          </button>
+        ))}
+      </div>
+    </header>
+  );
+}
