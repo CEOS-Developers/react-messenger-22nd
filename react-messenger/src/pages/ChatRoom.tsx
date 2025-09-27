a79550331b1247ad6f3f689af8020c6049a246d8
jungyungee <jungyungee0610@gmail.com>
2025-09-28 00:45:03 +0900

    [FIX] 날짜 수정 - 한국 기준

--- react-messenger/src/pages/ChatRoom.tsx
+++ react-messenger/src/pages/ChatRoom.tsx
@@ -21,7 +21,10 @@ const ChatRoom = () => {
 
   const handleSend = (text: string) => {
     const now = new Date();
-    const today = now.toISOString().split("T")[0];
+    const today = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(
+      2,
+      "0"
+    )}-${String(now.getDate()).padStart(2, "0")}`;
     const time = now.toLocaleTimeString("ko-KR", {
       hour: "2-digit",
       minute: "2-digit",
