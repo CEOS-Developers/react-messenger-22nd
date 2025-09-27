07a114108a703edde0543177e21eda1a6b309db5
fkgrkyr <fkgrkyr@gmail.com>
2025-09-27 19:16:14 +0900

    feat: 채팅목록 페이지 서치바 구현

--- src/pages/ChatList.tsx
+++ src/pages/ChatList.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/layout/Navbar";
 import ChatlistHeader from "@/components/header/ChatlistHeader";
+import Searchbar from "@/components/chatlist/Searchbar";
 
 const ChatList = () => {
   return (
-    <div className="flex flex-col h-full bg-gray-100">
+    <div className="flex flex-col h-full bg-gray-0">
       <ChatlistHeader />
+      <Searchbar />
       <div className="flex-1 flex flex-col items-center justify-center">
         <h1 className="text-2xl font-bold mb-4">채팅 목록 페이지</h1>
         <p className="text-gray-600">여기에 채팅 목록 표시</p>
