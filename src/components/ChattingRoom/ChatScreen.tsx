21b0731b1939cbbde59f768dc0b5dbe41fa08a8e
jy000n <jayoonchang@naver.com>
2025-09-27 17:21:00 +0900

    feat: 동일 사람/시간별 메시지 배치 스타일 추가

--- src/components/ChattingRoom/ChatScreen.tsx
+++ src/components/ChattingRoom/ChatScreen.tsx
@@ -25,6 +25,11 @@ const formatTime = (date: Date) => {
   return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
 };
 
+// minutes 일치 비교 (for rendering 채팅메시지)
+const isSameMinute = (d1: Date, d2: Date) => {
+  return d1.getHours() === d2.getHours() && d1.getMinutes() === d2.getMinutes();
+};
+
 const ChatScreen = () => {
   const [messages, setMessages] = useState<Message[]>([]);
 
@@ -42,29 +47,40 @@ const ChatScreen = () => {
     <div>
       {messages.map((msg, idx) => {
         const isMine = msg.senderId === MY_ID;
+        const prevMsg = idx > 0 ? messages[idx - 1] : null;
 
         // 날짜 비교
         const showDate = idx === 0 || formatDate(msg.sentAt) !== formatDate(messages[idx - 1].sentAt);
+
+        // 프로필+이름 표시 조건
+        const showProfileAndName =
+          !prevMsg ||
+          prevMsg.senderId !== msg.senderId ||
+          (prevMsg.senderId === msg.senderId && !isSameMinute(prevMsg.sentAt, msg.sentAt));
 
         return (
           <div key={msg.msgId} className="px-[20px] py-[8px]">
             {showDate && (
               <div className="flex justify-center">
-                <span className="rounded-[1000px] bg-[#ECEEF0] px-[23px] py-[5px] text-center text-[9px] font-normal text-[#6F7173]">
+                <span className="mb-[3px] rounded-[1000px] bg-[#ECEEF0] px-[23px] py-[5px] text-center text-[9px] font-normal text-[#6F7173]">
                   {formatDate(msg.sentAt)}
                 </span>{' '}
               </div>
             )}
             <div className="flex flex-row gap-[10px]">
-              {!isMine && <img src={DefaultProfile} alt={msg.senderName} className="h-[44px] w-[44px] rounded-[6px]" />}
+              {!isMine && showProfileAndName && (
+                <img src={DefaultProfile} alt={msg.senderName} className="h-[44px] w-[44px] rounded-[6px]" />
+              )}
               <div className="flex flex-col text-[11px]">
-                {!isMine && <span className="pt-[1px] font-semibold">{msg.senderName}</span>}
+                {!isMine && showProfileAndName && <span className="pt-[1px] font-semibold">{msg.senderName}</span>}
 
-                <div className="flex flex-row gap-[8px]">
-                  <span className="mt-[5px] rounded-[6px] bg-[#EBE4E0] px-[10px] py-[8px] font-normal">
-                    {msg.content}
-                  </span>
-                  <span className="mb-[1px] self-end font-normal text-[#888A8C]">{formatTime(msg.sentAt)}</span>
+                <div className={showProfileAndName ? '' : 'mt-[-15px] ml-[54px]'}>
+                  <div className="flex flex-row gap-[8px]">
+                    <span className="mt-[5px] rounded-[6px] bg-[#EBE4E0] px-[10px] py-[8px] font-normal">
+                      {msg.content}
+                    </span>
+                    <span className="mb-[1px] self-end font-normal text-[#888A8C]">{formatTime(msg.sentAt)}</span>
+                  </div>
                 </div>
               </div>
             </div>
